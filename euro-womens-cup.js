5609fd2b57f6a7859e00cae5ed8af6a16d9ad9d3
Joel Sempere Cobos <joelsemperecobos@gmail.com>
2022-07-13 00:54:30 +0200

    Added PRESENTACION EQUIPOS SEMIFINAL

---
+++ euro-womens-cup.js
@@ -0,0 +1,19 @@
+console.log('====================================================')
+console.log('=== COMIENZAN LAS FASES ELIMINATORIAS DEL TORNEO ===')
+console.log('====================================================')
+console.log()
+console.log('Equipos que participan en el playoff:')
+console.log()
+
+const grupoA = ['Noruega', 'Inglaterra']
+const grupoB = ['España', 'Alemania']
+const grupoC = ['Países Bajos', 'Suiza']
+const grupoD = ['Francia', 'Islandia']
+
+console.log(`GRUPO A: ${grupoA[0]}, ${grupoA[1]}`)
+console.log(`GRUPO B: ${grupoB[0]}, ${grupoB[1]}`)
+console.log(`GRUPO C: ${grupoC[0]}, ${grupoC[1]}`)
+console.log(`GRUPO D: ${grupoD[0]}, ${grupoD[1]}`)
+
+console.log()
+console.log('===== CUARTOS DE FINAL =====')
