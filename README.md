7128b9046c621ae80310b63ed54ac000c3f91411
Joel Sempere Cobos <joelsemperecobos@gmail.com>
2022-07-15 21:52:56 +0200

    Added match simulator function tie restricted

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Práctica de Fundamentos de JS
 
 Esta es la práctica del módulo de fundamentos de JS.
+
+## Simulación de partidos
+
+Para ofrecer resultados realistas, he optado por hacer una función basada en contadores. Generando un número aleatorio 10 veces, y haciendo que solo suba el marcador de uno u otro equipo si el número entra en un rango (si queda fuera, ningún equipo marca). Tras trastear un poco con los números llegué a unos resultados bastante realistas.
+
+Para implementar la **restricción de empates** he añadido un "gol de oro": si pasado el bucle ambos contadores son iguales, se repite el bucle hasta que sean diferentes.
--- index.js
+++ index.js
@@ -1,40 +1,76 @@
-console.log('====================================================')
-console.log('=== COMIENZAN LAS FASES ELIMINATORIAS DEL TORNEO ===')
-console.log('====================================================')
-console.log()
-console.log('Equipos que participan en el playoff:')
-console.log()
+console.log("====================================================");
+console.log("=== COMIENZAN LAS FASES ELIMINATORIAS DEL TORNEO ===");
+console.log("====================================================");
+console.log();
+console.log("Equipos que participan en el playoff:");
+console.log();
 
 // DECLARO GRUPOS COMO OBJETOS y los meto en un array
 
-const grupoA = {
-    name: "Grupo A",
-    teams: ['Noruega', 'Inglaterra'],
+class Grupos {
+  constructor(name, teams) {
+    this.name = name;
+    this.teams = teams;
+  }
 }
 
-const grupoB = {
-    name: "Grupo B",
-    teams: ['España', 'Alemania']
+const grupoA = new Grupos("Grupo A", ["Noruega", "Inglaterra"]);
+const grupoB = new Grupos("Grupo B", ["España", "Alemania"]);
+const grupoC = new Grupos("Grupo C", ["Países Bajos", "Suiza"]);
+const grupoD = new Grupos("Grupo D", ["Francia", "Islandia"]);
+
+const groups = [grupoA, grupoB, grupoC, grupoD];
+
+
+// SIMULACIÓN DE PARTIDOS
+
+function match(equipo1, equipo2){
+    let count1 = 0
+    let count2 = 0
+    let timmer=0
+    while (timmer < 10){
+        const randomNum = Math.random()
+        if (0.65 < randomNum && randomNum < 0.8){
+            count1++
+        } else if (0.8 < randomNum && randomNum < 0.95) {
+            count2++
+        }         
+        timmer ++
+    }
+
+    while (count1 == count2){
+        const randomNum = Math.random()
+        if (0.65 < randomNum && randomNum < 0.8){
+            count1++
+        } else if (0.8 < randomNum && randomNum < 0.95) {
+            count2++
+        }         
+    }
+
+    const winner = whoWins(equipo1, equipo2)
+    function whoWins(equipo1, equipo2){
+       
+        if (count1 > count2){
+            return equipo1
+        } else {
+            return equipo2
+        }
+    }
+
+    console.log(`${equipo1} ${count1} - ${count2} ${equipo2} => ${winner} `)
+    return count1, count2
 }
 
-const grupoC = {
-    name: "Grupo C",
-    teams: ['Países Bajos', 'Suiza']
-}
-
-const grupoD = {
-    name: "Grupo D",
-    teams: ['Francia', 'Islandia']
-}
-
-const groups = [grupoA, grupoB, grupoC, grupoD]
 
 
 // MUESTRO LOS EQUIPOS EN CADA GRUPO
 
-for (group of groups){
-    console.log(`${group.name}: ${group.teams[0]}, ${group.teams[1]}`)
+for (group of groups) {
+  console.log(`${group.name}: ${group.teams[0]}, ${group.teams[1]}`);
 }
 
-console.log()
-console.log('===== CUARTOS DE FINAL =====')
+
+console.log();
+console.log("===== CUARTOS DE FINAL =====");
+
+match(grupoA.teams[0], grupoB.teams[1])
