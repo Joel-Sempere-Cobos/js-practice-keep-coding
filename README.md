ffdf97aa88342d0de0c61e7fe93dac6336026a5d
Joel-Sempere-Cobos <91223315+Joel-Sempere-Cobos@users.noreply.github.com>
2022-07-25 11:14:57 +0200

    OBLIGATORIO COMPLETADO

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 Esta es la práctica del módulo de fundamentos de JS.
 
+## Asignación aleatoria de equipos a los grupos
+
+He creado un constructor de equipos, y tras crearlos los he metido en un array de 16 equipos, que se desordenan y se reparten 4 en cada grupo, de los cuales luego solo presento 2, que serán los que han llegado a cuartos.
+
 ## Simulación de partidos
 
-Para ofrecer resultados realistas, he optado por hacer una función basada en *contadores*. Generando un *número aleatorio 10 veces*, y haciendo que solo suba el marcador de uno u otro equipo si el número *entra en un rango* (si queda fuera, ningún equipo marca). Tras trastear un poco con los números llegué a unos resultados bastante realistas.
+Para ofrecer resultados más realistas, en lugar de utilizar simplemente valores aleatorios he optado por hacer una función basada en **contadores**. Generando un *número aleatorio 10 veces*, y haciendo que solo suba el marcador de uno u otro equipo si el número *entra en un rango* (si queda fuera, ningún equipo marca). Tras trastear un poco con los números llegué a unos resultados bastante realistas.
 
-Para implementar la **restricción de empates** he añadido un "gol de oro": si pasado el bucle ambos contadores son iguales, se repite el bucle hasta que sean diferentes.
+Para implementar la **restricción de empates** he añadido un *gol de oro*: si pasado el bucle ambos contadores son iguales, se repite el bucle hasta que sean diferentes.
+
+La función retorna un objeto con las propiedades ``winner`` y ``loser`` que utilizo para seleccionar y preparar el siguiente partido.
--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-
-
 // VARIABLES
 
-let teamList = []
+let teamList = [];
 
 const liga = {
     name: "Euro Women's League",
     teams: teamList,
-    rounds: []
-}
+    rounds: [],
+};
 
 class Grupos {
     constructor(name, teams) {
@@ -17,94 +15,86 @@ class Grupos {
     }
 }
 
-import {Team} from './classes/Teams.js'
+import { Team } from "./classes/Teams.js";
 
+let spain = new Team("España");
+let england = new Team("Inglaterra");
+let netherlands = new Team("Países Bajos");
+let germany = new Team("Alemania");
+let denmark = new Team("Dinamarca");
+let normay = new Team("Noruega");
+let sweden = new Team("Suecia");
+let france = new Team("Francia");
+let belgium = new Team("Bélgica");
+let iceland = new Team("Islandia");
+let finland = new Team("Finlandia");
+let austria = new Team("Austria");
+let italy = new Team("Italia");
+let switzerland = new Team("Suiza");
+let northernIreland = new Team("Irlanda del Norte");
+let portugal = new Team("Portugal");
 
-let spain = new Team("España")
-let england = new Team("Inglaterra")
-let netherlands = new Team("Países Bajos")
-let germany = new Team("Alemania")
-let denmark = new Team("Dinamarca")
-let normay = new Team("Noruega")
-let sweden = new Team("Suecia")
-let france = new Team("Francia")
-let belgium = new Team("Bélgica")
-let iceland = new Team("Islandia")
-let finland = new Team("Finlandia")
-let austria = new Team("Austria")
-let italy = new Team("Italia")
-let switzerland = new Team("Suiza")
-let northernIreland = new Team("Irlanda del Norte")
-let portugal = new Team("Portugal")
-
-
-
-let team1
-let team2
-
+let team1;
+let team2;
 
 // PROCESOS
 
+teamList.push(spain);
+teamList.push(england);
+teamList.push(netherlands);
+teamList.push(germany);
+teamList.push(denmark);
+teamList.push(normay);
+teamList.push(sweden);
+teamList.push(france);
+teamList.push(belgium);
+teamList.push(iceland);
+teamList.push(finland);
+teamList.push(austria);
+teamList.push(italy);
+teamList.push(switzerland);
+teamList.push(northernIreland);
+teamList.push(portugal);
 
-teamList.push(spain)
-teamList.push(england)
-teamList.push(netherlands)
-teamList.push(germany)
-teamList.push(denmark)
-teamList.push(normay)
-teamList.push(sweden)
-teamList.push(france)
-teamList.push(belgium)
-teamList.push(iceland)
-teamList.push(finland)
-teamList.push(austria)
-teamList.push(italy)
-teamList.push(switzerland)
-teamList.push(northernIreland)
-teamList.push(portugal)
+teamList.sort(function () {
+    return Math.random() - 0.5;
+});
 
-teamList.sort(function() {return Math.random() - 0.5})
-
-
-
-
-const grupoA = new Grupos("Grupo A", teamList.splice(0,4));
-const grupoB = new Grupos("Grupo B", teamList.splice(0,4));
-const grupoC = new Grupos("Grupo C", teamList.splice(0,4));
-const grupoD = new Grupos("Grupo D", teamList.splice(0,4));
+const grupoA = new Grupos("Grupo A", teamList.splice(0, 4));
+const grupoB = new Grupos("Grupo B", teamList.splice(0, 4));
+const grupoC = new Grupos("Grupo C", teamList.splice(0, 4));
+const grupoD = new Grupos("Grupo D", teamList.splice(0, 4));
 
 const groups = [grupoA, grupoB, grupoC, grupoD];
-
 
 // SIMULACIÓN DE PARTIDOS
 
 function match(equipo1, equipo2) {
-  let count1 = 0;
-  let count2 = 0;
-  let timmer = 0;
+    let count1 = 0;
+    let count2 = 0;
+    let timmer = 0;
 
-  const team1min = 0.68;
-  const team1max = 0.8;
-  const team2min = 0.8;
-  const team2max = 0.92;
+    const team1min = 0.6;
+    const team1max = 0.8;
+    const team2min = 0.8;
+    const team2max = 1;
 
-    while (timmer < 10) {
+    function contarGoles() {
         const randomNum = Math.random();
         if (team1min < randomNum && randomNum < team1max) {
-        count1++;
+            count1++;
         } else if (team2min < randomNum && randomNum < team2max) {
-        count2++;
+            count2++;
         }
+    }
+
+    while (timmer < 10) {
+        contarGoles();
         timmer++;
     }
 
     while (count1 == count2) {
-        const randomNum = Math.random();
-        if (team1min < randomNum && randomNum < team1max) {
-        count1++;
-        } else if (team2min < randomNum && randomNum < team2max) {
-        count2++;
-        }
+        contarGoles();
     }
 
     function whoWins(equipo1, equipo2) {
@@ -114,17 +104,26 @@ function match(equipo1, equipo2) {
             return equipo2;
         }
     }
+
+    function whoLose(equipo1, equipo2) {
+        if (count1 < count2) {
+            return equipo1;
+        } else {
+            return equipo2;
+        }
+    }
+
     const winner = whoWins(equipo1, equipo2);
+    const loser = whoLose(equipo1, equipo2);
 
     console.log(`${equipo1} ${count1} - ${count2} ${equipo2} => ${winner} `);
+
+    return { winner, loser };
 }
 
+// OPCIONAL
 
-
-// PRESENTACIÓN
-
-
-console.log("Grupos y equipos")
+/* console.log("Grupos y equipos")
 console.log("===============================")
 
 for (let group of groups) {
@@ -135,9 +134,9 @@ for (let group of groups) {
     }
     console.log()
 }
+ */
 
-
-
+//OBLIGATORIO
 
 console.log("====================================================");
 console.log("=== COMIENZAN LAS FASES ELIMINATORIAS DEL TORNEO ===");
@@ -146,19 +145,37 @@ console.log();
 console.log("Equipos que participan en el playoff:");
 console.log();
 
-
-
 for (let group of groups) {
-    console.log(`${group.name}: ${group.teams[0].name}, ${group.teams[1].name}`);
+    console.log(
+        `${group.name}: ${group.teams[0].name}, ${group.teams[1].name}`
+    );
 }
 
 console.log();
 console.log("===== CUARTOS DE FINAL =====");
 
-match(grupoA.teams[0].name, grupoB.teams[1].name);
-match(grupoB.teams[0].name, grupoA.teams[1].name);
-match(grupoC.teams[0].name, grupoD.teams[1].name);
-match(grupoD.teams[0].name, grupoC.teams[1].name);
+const c1 = match(grupoA.teams[0].name, grupoB.teams[1].name);
+const c2 = match(grupoB.teams[0].name, grupoA.teams[1].name);
+const c3 = match(grupoC.teams[0].name, grupoD.teams[1].name);
+const c4 = match(grupoD.teams[0].name, grupoC.teams[1].name);
 
 console.log();
 console.log("===== SEMIFINALES =====");
+
+const s1 = match(c1.winner, c3.winner);
+const s2 = match(c2.winner, c4.winner);
+
+console.log();
+console.log("===== TERCER Y CUARTO PUESTO =====");
+
+const subChampion = match(s1.loser, s2.loser);
+
+console.log();
+console.log("===== FINAL =====");
+
+const champion = match(s1.winner, s2.winner);
+
+console.log();
+console.log("===============================================");
+console.log(`¡${champion.winner} campeona de la EURO WOMEN’S CUP!`);
+console.log("===============================================");
